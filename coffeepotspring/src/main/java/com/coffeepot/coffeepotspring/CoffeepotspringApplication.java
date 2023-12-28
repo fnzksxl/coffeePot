2d4849e7e1bd37d3f25a4488ac90f498c67ab297
5almiakki <jpui9635@gmail.com>
2023-12-28 17:28:34 +0900

    Init: 스프링 부트 프로젝트 초기화 #5

---
+++ coffeepotspring/src/main/java/com/coffeepot/coffeepotspring/CoffeepotspringApplication.java
@@ -0,0 +1,13 @@
+package com.coffeepot.coffeepotspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeepotspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeepotspringApplication.class, args);
+	}
+
+}
---
+++ coffeepotspring/src/test/java/com/coffeepot/coffeepotspring/CoffeepotspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeepot.coffeepotspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeepotspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
