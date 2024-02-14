84381788097a951da2d5878caba9e2a1e93faaf0
5almiakki <jpui9635@gmail.com>
2024-02-14 17:46:55 +0900

    Fix: 빌드 시 에러를 없애기 위해 빈 테스트 메소드 제거

--- coffeepotspring/src/test/java/com/coffeepot/coffeepotspring/CoffeepotspringApplicationTests.java
+++ coffeepotspring/src/test/java/com/coffeepot/coffeepotspring/CoffeepotspringApplicationTests.java
@@ -1,13 +1,8 @@
 package com.coffeepot.coffeepotspring;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoffeepotspringApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
