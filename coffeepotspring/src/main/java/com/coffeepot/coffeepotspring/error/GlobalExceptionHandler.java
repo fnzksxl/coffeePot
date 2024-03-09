ae24b22a3e8753ac7047d55830a8d661499f17c9
5almiakki <jpui9635@gmail.com>
2024-03-10 04:24:34 +0900

    Fix: 일반적인 예외에 관한 응답 수정 #16

--- coffeepotspring/src/main/java/com/coffeepot/coffeepotspring/error/GlobalExceptionHandler.java
+++ coffeepotspring/src/main/java/com/coffeepot/coffeepotspring/error/GlobalExceptionHandler.java
@@ -1,9 +1,29 @@
 package com.coffeepot.coffeepotspring.error;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.coffeepot.coffeepotspring.dto.ResponseDTO;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatusCode statusCode, WebRequest request) {
+		ResponseDTO<Object> response = ResponseDTO.builder().error(ex.getMessage()).build();
+		return ResponseEntity.status(statusCode.value()).body(response);
+	}
+	
+	@ExceptionHandler
+	protected ResponseEntity<?> handleException(Exception e) {
+		ResponseDTO<Object> response = ResponseDTO.builder().error(e.getMessage()).build();
+		return ResponseEntity.badRequest().body(response);
+	}
 
 }
