2022302c4531a6a51287be9e5ab0782948b54738
5almiakki <jpui9635@gmail.com>
2024-03-19 23:47:10 +0900

    Docs: SearchController API 작성 #17

--- coffeepotspring/src/main/java/com/coffeepot/coffeepotspring/controller/SearchController.java
+++ coffeepotspring/src/main/java/com/coffeepot/coffeepotspring/controller/SearchController.java
@@ -2,6 +2,7 @@ package com.coffeepot.coffeepotspring.controller;
 
 import java.util.List;
 
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,6 +15,9 @@ import com.coffeepot.coffeepotspring.dto.MemoSearchParamDTO;
 import com.coffeepot.coffeepotspring.dto.ResponseDTO;
 import com.coffeepot.coffeepotspring.service.MemoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,10 +28,18 @@ import lombok.extern.slf4j.Slf4j;
 public class SearchController {
 	
 	private final MemoService memoService;
-	
+
+	@Operation(summary = "메모 검색", description = "titleKeyword: 제목 검색 키워드\n\n"
+			+ "contentKeyword: 내용 검색 키워드\n\n"
+			+ "authorKeyword: 글쓴이 검색 키워드")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "메모 검색 성공"),
+		@ApiResponse(responseCode = "400", description = "메모 검색 실패"),
+		@ApiResponse(responseCode = "404", description = "메모를 찾을 수 없음")
+	})
 	@GetMapping("/memo")
 	public ResponseEntity<?> searchMemoList(
-			@ModelAttribute MemoSearchParamDTO memoSearchParamDTO,
+			@ModelAttribute @ParameterObject MemoSearchParamDTO memoSearchParamDTO,
 			@RequestParam(defaultValue = "") String memoId,
 			@RequestParam(defaultValue = "5") int pageSize,
 			@RequestParam(defaultValue = "createdAt") String sortBy) {
